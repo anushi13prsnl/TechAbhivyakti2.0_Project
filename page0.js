681d7693e7d4200f9c97affafcfcfbb68ff137dd
“Anuj-er” <anujkumar142000@gmail.com>
2024-03-17 10:51:05 +0530

    Added Login Feature and also add a sign out button in nav bar

---
+++ page0.js
@@ -0,0 +1,115 @@
+import {firebaseConfig} from './config.js';
+
+// Commenting all the things
+// Firebase configuration
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const signupForm = document.querySelector('.registration.form');
+const loginForm = document.querySelector('.login.form');
+const forgotForm=document.querySelector('.forgot.form');
+const container=document.querySelector('.container');
+const signupBtn = document.querySelector('.signupbtn');
+const anchors = document.querySelectorAll('a');
+
+// Event listeners
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', () => {
+    // Get the id of the anchor
+    const id = anchor.id;
+    switch(id){
+    case 'loginLabel':
+        signupForm.style.display = 'none';
+        loginForm.style.display = 'block';
+        forgotForm.style.display = 'none';
+        break;
+      case 'signupLabel':
+        signupForm.style.display = 'block';
+        loginForm.style.display = 'none';
+        forgotForm.style.display = 'none';
+        break;
+      case 'forgotLabel':
+        signupForm.style.display = 'none';
+        loginForm.style.display = 'none';
+        forgotForm.style.display = 'block';
+        break;
+    }
+  });
+});
+
+// Event listeners
+signupBtn.addEventListener('click', () => {
+  // Get the values of the input fields
+  const name = document.querySelector('#name').value;
+  const username = document.querySelector('#username').value;
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value;
+  // Create a new user
+  auth.createUserWithEmailAndPassword(email, password)
+  // Save the user data to Firestore
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const uid = user.uid;
+        user.sendEmailVerification()
+        .then(() => {
+          alert('Verification email sent. Please check your inbox and verify your email before signing in.');
+        })
+        .catch((error) => {
+          alert('Error sending verification email: ' + error.message);
+        });
+        // Save the user data to Firestore
+        console.log('User data saved to Firestore');
+        firestore.collection('users').doc(uid).set({
+          name: name,
+          username: username,
+          email: email,
+      })
+      
+      // Hide the signup form and show the login form
+        signupForm.style.display = 'none';
+        loginForm.style.display = 'block';
+        forgotForm.style.display = 'none';
+    })
+    .catch((error) => {
+      alert('Error signing up: '+error.message);
+    });
+});
+
+// Event listeners
+const loginBtn = document.querySelector('.loginbtn');
+loginBtn.addEventListener('click', () => {
+  const email = document.querySelector('#inUsr').value.trim();
+  const password = document.querySelector('#inPass').value;
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      if (user.emailVerified) {
+        console.log('User is signed in with a verified email.');
+        location.href = "main.html";
+      } else {
+        alert('Please verify your email before signing in.');
+      }
+    })
+    .catch((error) => {
+      alert('Error signing in: ' + error.message);
+    });
+});
+
+// Event listeners
+const forgotBtn=document.querySelector('.forgotbtn');
+forgotBtn.addEventListener('click', () => {
+  const emailForReset = document.querySelector('#forgotinp').value.trim();
+ if (emailForReset.length>0) {
+   auth.sendPasswordResetEmail(emailForReset)
+ .then(() => {
+   alert('Password reset email sent. Please check your inbox to reset your password.');
+        signupForm.style.display = 'none';
+        loginForm.style.display = 'block';
+        forgotForm.style.display = 'none';
+    })
+    .catch((error) => {
+    alert('Error sending password reset email: ' + error.message);
+  });
+  }
+});
+
